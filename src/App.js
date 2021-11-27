5a4a60bbcd0cae992f5c270505954e80b9da3eeb
Sourav <kediasourav2@gmail.com>
2021-11-27 17:41:59 +0530

    Added routes to frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      {/* Hello there */}
-      {/* <Register /> */}
-      <Login />
+      Hi there!!!
+      <BrowserRouter>
+      <Routes>
+        
+        <Route path = "/register" element = {<Register />} />
+        <Route path = "/login" element = {<Login />} />
+      
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
